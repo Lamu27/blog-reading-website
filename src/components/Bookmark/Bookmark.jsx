7b103863650da83e403c47e73586ac2cf8219e76
Lamu27 <lamisasharmin2@gmail.com>
2025-04-24 23:05:34 +0600

    modified bookmarks class properties

--- src/components/Bookmark/Bookmark.jsx
+++ src/components/Bookmark/Bookmark.jsx
@@ -3,8 +3,8 @@ import PropTypes from "prop-types"
 export default function Bookmark({bookmark} ) {
     const {title} = bookmark;
   return (
-    <div className="bg-cyan-100 m-5 p-1 rounded-lg">
-        <h3 className="text-lg font-bold text-cyan-950 p-2">{title}</h3>
+    <div className="bg-cyan-100 m-5 rounded-lg">
+        <h3 className="text-lg font-bold text-cyan-950 pl-2">{title}</h3>
     </div>
     
   )
