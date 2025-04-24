a1eebead3f0d4fb41beca6ab8ffd3c5aa35f2b0b
Lamu27 <lamisasharmin2@gmail.com>
2025-04-24 23:00:28 +0600

    edited title background

--- src/components/Bookmark/Bookmark.jsx
+++ src/components/Bookmark/Bookmark.jsx
@@ -3,8 +3,8 @@ import PropTypes from "prop-types"
 export default function Bookmark({bookmark} ) {
     const {title} = bookmark;
   return (
-    <div className="bg-green-100 m-5 p-3 rounded-lg">
-        <h3 className="text-lg font-bold text-emerald-950">{title}</h3>
+    <div className="bg-cyan-300 m-5 p-2 rounded-lg">
+        <h3 className="text-lg font-bold text-cyan-950">{title}</h3>
     </div>
     
   )
