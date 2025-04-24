603986b98d3797a6d219b88568c52ab5b9eaedc0
Lamu27 <lamisasharmin2@gmail.com>
2025-04-24 23:07:33 +0600

    correcting color scheme with padding

--- src/components/Bookmark/Bookmark.jsx
+++ src/components/Bookmark/Bookmark.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 export default function Bookmark({bookmark} ) {
     const {title} = bookmark;
   return (
-    <div className="bg-cyan-100 m-5 rounded-lg">
+    <div className="bg-cyan-100 mx-5 p-1 rounded-lg">
         <h3 className="text-lg font-bold text-cyan-950 pl-2">{title}</h3>
     </div>
     
